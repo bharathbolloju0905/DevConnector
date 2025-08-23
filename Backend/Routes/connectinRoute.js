4c9f9afc718ce8eb0381a3cfbfbf64fde6955306
Bharath Bolloju <22tq1a6659@siddhartha.co.in>
2025-08-23 20:26:40 +0530

    "resolved requirement issue"

--- Backend/Routes/connectinRoute.js
+++ Backend/Routes/connectinRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authenticate = require("../middlewares/authenticateMiddleware");
-const ConnectionControllers = require("../controllers/connectionController");
+const ConnectionControllers = require("../Controllers/connectionController");
 
 router.post("/connect",authenticate.authenticate,ConnectionControllers.ConnectUser)
 
